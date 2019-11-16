4b0daa80d4329a373e22146a5db8370393f44928
ianDesson <ian.desson@outlook.com>
2019-11-16 01:14:45 -0500

    Add Access_Token Request

--- src/services/api/getAccessToken.js
+++ src/services/api/getAccessToken.js
@@ -1,11 +1,28 @@
-import axios from 'axios';
-require('dotenv').config();
+const axios = require("axios");
+require("dotenv").config();
 
-
-
+/*
+ * Note: This requires setting a .env file in the root directory
+ * that specifies your API_KEY
+ */
 const getAccessToken = () => {
-  axios.post('https://api.wrnch.ai/v1/login', {
-    'Content-Type': 'application/json',
-    'api-key': process.env.API_KEY,
-  });
+  console.log('POST for access_token');
+  axios
+    .post("https://api.wrnch.ai/v1/login", { api_key: process.env.API_KEY })
+    .then(response => {
+      // Passed
+      console.log('Response status:',response.status, response.statusText);
+      return response.data.access_token;
+    })
+    .catch(error => {
+      // Error
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      if (error.request) {
+        console.log(error.request);
+      }
+    });
 };
+
+export default getAccessToken;
